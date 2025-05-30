858028a7aa7a0d15191b51746d7cd09d85f021e4
Neil Butcher <n.a.butcher@bham.ac.uk>
2025-05-30 15:19:53 +0100

    tests to check results against each other

---
+++ multiroom_model_tests/test_class_results_match.py
@@ -0,0 +1,158 @@
+import unittest
+import pickle
+from multiroom_model.inchemrunner import InChemPyRunner
+from multiroom_model.inchem import InChemPyInstance
+from modules.initial_dictionaries_class import InititialTxtFile, InitialPickleFile, InitialDataFrame
+from numpy.testing import assert_allclose
+
+
+class TestInChemPyClassResults(unittest.TestCase):
+    # def test_run_inchem_py_to_get_results(self):
+    #    inchem_py_instance: InChemPyInstance = InChemPyInstance(
+    #        particles=False,
+    #        INCHEM_additional=False,
+    #        seconds_to_integrate=360,
+    #        output_graph=False)
+    #
+    #    inchem_py_instance.run()
+
+    def benchmark_results(self):
+        with open('multiroom_model_tests/test_run_inchem_py_to_get_results.pickle','rb') as file:
+            result = pickle.load(file)
+        return result
+
+    def test_single_phase_run_results_repeatable(self):
+        inchem_py_runner: InChemPyRunner = InChemPyRunner(
+            particles=False,
+            INCHEM_additional=False,
+            output_graph=False)
+
+        initial_conditions = InititialTxtFile('initial_concentrations.txt')
+
+        output_concentrations_A, _ = inchem_py_runner.run(
+            initial_conditions=initial_conditions, seconds_to_integrate=180)
+
+        output_concentrations_B, _ = inchem_py_runner.run(
+            initial_conditions=initial_conditions, seconds_to_integrate=180)
+
+        self.assertEqual(4, len(output_concentrations_A))
+        self.assertEqual(4, len(output_concentrations_B))
+
+        for species in inchem_py_runner.species():
+            self.assertEqual(output_concentrations_A.get(species)[0.0],
+                             output_concentrations_B.get(species)[0.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations_A.get(species)[60.0],
+                             output_concentrations_B.get(species)[60.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations_A.get(species)[120.0],
+                             output_concentrations_B.get(species)[120.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations_A.get(species)[180.0],
+                             output_concentrations_B.get(species)[180.0], f"species check failed: {species}")
+
+    def test_single_phase_run_results_match_double_phase(self):
+        inchem_py_runner: InChemPyRunner = InChemPyRunner(
+            particles=False,
+            INCHEM_additional=False,
+            output_graph=False)
+
+        initial_conditions = InititialTxtFile('initial_concentrations.txt')
+
+        output_concentrations_A1, _ = inchem_py_runner.run(
+            initial_conditions=initial_conditions, seconds_to_integrate=180)
+
+        initial_conditions_b = InitialDataFrame(output_concentrations_A1)
+
+        output_concentrations_A2, _ = inchem_py_runner.run(
+            initial_conditions=initial_conditions_b, t0=180, seconds_to_integrate=180)
+
+        output_concentrations_B, _ = inchem_py_runner.run(
+            initial_conditions=initial_conditions, seconds_to_integrate=360)
+
+        self.assertEqual(4, len(output_concentrations_A1))
+        self.assertEqual(4, len(output_concentrations_A2))
+        self.assertEqual(7, len(output_concentrations_B))
+
+        for species in inchem_py_runner.species():
+            self.assertEqual(output_concentrations_A1.get(species)[0.0],
+                             output_concentrations_B.get(species)[0.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations_A1.get(species)[60.0],
+                             output_concentrations_B.get(species)[60.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations_A1.get(species)[120.0],
+                             output_concentrations_B.get(species)[120.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations_A1.get(species)[180.0],
+                             output_concentrations_B.get(species)[180.0], f"species check failed: {species}")
+
+            assert_allclose(output_concentrations_A2.get(species)[180.0],
+                            output_concentrations_B.get(species)[180.0],
+                            rtol=1.0e-5, err_msg=f"species check failed: {species}")
+            assert_allclose(output_concentrations_A2.get(species)[240.0],
+                            output_concentrations_B.get(species)[240.0],
+                            rtol=1.0e-5, err_msg=f"species check failed: {species}")
+            assert_allclose(output_concentrations_A2.get(species)[300.0],
+                            output_concentrations_B.get(species)[300.0],
+                            rtol=1.0e-5, err_msg=f"species check failed: {species}")
+            assert_allclose(output_concentrations_A2.get(species)[360.0],
+                            output_concentrations_B.get(species)[360.0],
+                            rtol=1.0e-5, err_msg=f"species check failed: {species}")
+
+    def test_single_phase_run_matches_instance(self):
+        inchem_py_runner: InChemPyRunner = InChemPyRunner(
+            particles=False,
+            INCHEM_additional=False,
+            output_graph=False)
+
+        initial_conditions = InititialTxtFile('initial_concentrations.txt')
+
+        output_concentrations, _ = inchem_py_runner.run(
+            initial_conditions=initial_conditions, seconds_to_integrate=360)
+
+        benchmark_concentrations = self.benchmark_results()
+
+        for species in inchem_py_runner.species():
+            self.assertEqual(output_concentrations.get(species)[0.0],
+                             benchmark_concentrations.get(species)[0.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations.get(species)[60.0],
+                             benchmark_concentrations.get(species)[60.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations.get(species)[120.0],
+                             benchmark_concentrations.get(species)[120.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations.get(species)[180.0],
+                             benchmark_concentrations.get(species)[180.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations.get(species)[240.0],
+                             benchmark_concentrations.get(species)[240.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations.get(species)[300.0],
+                             benchmark_concentrations.get(species)[300.0], f"species check failed: {species}")
+            self.assertEqual(output_concentrations.get(species)[360.0],
+                             benchmark_concentrations.get(species)[360.0], f"species check failed: {species}")
+
+    def test_double_phase_run_matches_instance(self):
+        inchem_py_runner: InChemPyRunner = InChemPyRunner(
+            particles=False,
+            INCHEM_additional=False,
+            output_graph=False)
+
+        initial_conditions = InititialTxtFile('initial_concentrations.txt')
+
+        interim_concentrations, _ = inchem_py_runner.run(
+            initial_conditions=initial_conditions, seconds_to_integrate=180)
+        output_concentrations, _ = inchem_py_runner.run(
+            initial_conditions=InitialDataFrame(interim_concentrations), t0=180, seconds_to_integrate=180)
+
+        benchmark_concentrations = self.benchmark_results()
+
+        for species in inchem_py_runner.species():
+            self.assertEqual(interim_concentrations.get(species)[0.0],
+                             benchmark_concentrations.get(species)[0.0], f"species check failed: {species}")
+            self.assertEqual(interim_concentrations.get(species)[60.0],
+                             benchmark_concentrations.get(species)[60.0], f"species check failed: {species}")
+            self.assertEqual(interim_concentrations.get(species)[120.0],
+                             benchmark_concentrations.get(species)[120.0], f"species check failed: {species}")
+            self.assertEqual(interim_concentrations.get(species)[180.0],
+                             benchmark_concentrations.get(species)[180.0], f"species check failed: {species}")
+            assert_allclose(output_concentrations.get(species)[240.0],
+                            benchmark_concentrations.get(species)[240.0],
+                            rtol=1.0e-3, atol=1.0e-20, err_msg=f"species check failed: {species}")
+            assert_allclose(output_concentrations.get(species)[300.0],
+                            benchmark_concentrations.get(species)[300.0],
+                            rtol=1.0e-3, atol=1.0e-20, err_msg=f"species check failed: {species}")
+            assert_allclose(output_concentrations.get(species)[360.0],
+                            benchmark_concentrations.get(species)[360.0],
+                            rtol=1.0e-3, atol=1.0e-20, err_msg=f"species check failed: {species}")
